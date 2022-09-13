04ad7fe11c4ac74ceb6aac1c4906199151d0264b
Dannytech007 <okeagud@gmail.com>
2022-09-12 23:12:34 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "willson"
+/**
+ * main - Prints Holberton
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('w');
+	_putchar('i');
+	_putchar('l');
+	_putchar('l');
+	_putchar('s');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
