303b99852f91b6f1a8987eb1232aaf46af9e1992
Dannytech007 <okeagud@gmail.com>
2022-09-14 13:05:47 -0700

    Task O

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,44 @@
-#include "holberton.h"
+#include "main.h"
+
+
+
 /**
- * main - Prints Holberton
+
+ * main - prints _putchar
+
  *
- * Return: Always 0 (Success)
+
+ * Description: prints _putchar
+
+ *
+
+ * Return: 0 (Success)
+
  */
+
+
+
 int main(void)
+
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+
+	char text[9] = "_putchar";
+
+	int i = 0;
+
+
+
+	for (i = 0; i < 8; i++)
+
+	{
+
+		_putchar(text[i]);
+
+	}
+
 	_putchar('\n');
+
+
+
 	return (0);
-}
+
