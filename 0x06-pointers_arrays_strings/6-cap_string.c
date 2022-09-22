23971f2d7c9654d5205ed8a11a36b45fb980774d
Dannytech007 <okeagud@gmail.com>
2022-09-22 13:54:08 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,8 @@ char *cap_string(char *s)
 /*  scan through string */
 	   count =  0;
 	   while (s[count] != '\0')
-	   {/* if next character after count is a char , capitalise it */
+	   {
+		   /* if next character after count is a char , capitalise it */
 		   if (s[0] >= 97 && s[0] <= 122)
 		   {
 			   s[0] = s[0] - 32;
@@ -22,8 +23,8 @@ char *cap_string(char *s)
 		   if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
 				   || s[count] == ',' || s[count] == ';' || s[count] == '.'
 				   || s[count] == '.' || s[count] == '!' || s[count] == '?'
-				     || s[count] == '"' || s[count] == '(' || s[count] == ')'
-				      || s[count] == '{' || s[count] == '}')
+		        	   || s[count] == '"' || s[count] == '(' || s[count] == ')'
+	      		           || s[count] == '{' || s[count] == '}')
 		   {
 			   if (s[count + 1] >= 97 && s[count + 1] <= 122)
 				  {
